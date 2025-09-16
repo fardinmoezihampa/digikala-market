07c0f19030ca04c5d835817b527d83170b8317d2
fardinmoezi <fardinm2000@gmail.com>
2025-09-17 01:29:40 +0330

    Migration and Models done

---
+++ database/migrations/2025_09_16_200420_create_category_feature_values_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_feature_values', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('category_feature_id')->nullable()->constrained();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_feature_values');
+    }
+};
